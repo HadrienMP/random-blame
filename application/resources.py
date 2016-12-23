cfaf771648e5bc94a0be4397c4f027f2d357b594
Hadrien Mens-Pellen <public@hadrienmp.fr>
2016-12-23 08:56:07 +0000

    Make patch work for the first unit test

--- application/resources.py
+++ application/resources.py
@@ -1,11 +1,11 @@
 from application import app
 from flask import json
-from application.service import random_person
+from application import service
 
 
 @app.route("/", methods=["POST"])
 def blame():
-    message = "I blame @" + random_person(from_room='toto') + "! >:-("
+    message = "I blame @" + service.random_person(from_room='toto') + "! >:-("
     return json.jsonify({
             "color": "red",
             "message": message,
